029674bab825e06ad02ba7c36ab5c8775890e1ef
Rasheed Babatunde <brasheed240@gmail.com>
2024-12-17 22:47:55 +0100

    task action urls

--- eldertasks/urls.py
+++ eldertasks/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from . import views
 from .views import (
     TaskListView, TaskDetailView, TaskCreateView, TaskUpdateView, TaskDeleteView, TaskMediaUploadView,
     TaskApplicationCreateView, ReviewCreateView, task_media_delete, TaskMediaUploadCreateView
@@ -17,5 +18,10 @@ urlpatterns = [
     path("<uuid:task_id>/apply/", TaskApplicationCreateView.as_view(), name="task_apply"),
     path("<uuid:task_id>/review/", ReviewCreateView.as_view(), name="task_review"),
     path("media/<int:pk>/delete/", task_media_delete, name="task_media_delete"),
+
+    path('applications/<uuid:application_id>/accept/', views.accept_application, name='accept_application'),
+    path('applications/<uuid:application_id>/reject/', views.reject_application, name='reject_application'),
+    path('applications/<uuid:application_id>/withdraw/', views.withdraw_application, name='withdraw_application'),
+    path('tasks/<uuid:task_id>/complete/', views.mark_task_complete, name='mark_task_complete'),
     
 ]
