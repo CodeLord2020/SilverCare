ca5b1e9aaf7fcb4360a8c8f26eae350347f9bf7e
CodeLord2020 <brasheed240@gmail.com>
2025-01-03 12:29:08 +0100

    apps not ready fix

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,16 @@
 from django.apps import AppConfig
+import cloudinary
+from decouple import config
 
 
 class AccountsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "accounts"
+
+    def ready(self):
+        # Configure Cloudinary only once the app is ready
+        cloudinary.config(
+            cloud_name=config('CLOUD_NAME'),
+            api_key=config("API_KEY"),
+            api_secret=config("API_SECRET"),
+        )
