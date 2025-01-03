707c79ad1008d965c1ecbc7c34ca0de0b4893316
CodeLord2020 <brasheed240@gmail.com>
2025-01-03 12:27:33 +0100

    asgi apps not ready fix

--- silvercare/asgi.py
+++ silvercare/asgi.py
@@ -12,16 +12,15 @@ import os
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "silvercare.settings")
-
+django_asgi_app = get_asgi_application()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
 from .middleware import SessionAuthMiddleware
 from messaging.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AllowedHostsOriginValidator(
         SessionAuthMiddleware(
             URLRouter(websocket_urlpatterns)
