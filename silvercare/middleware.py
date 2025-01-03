281109e6ab7b250c6bd7c4e51fbc019e08615e82
CodeLord2020 <brasheed240@gmail.com>
2025-01-03 12:27:51 +0100

    middleware apps not ready fix

--- silvercare/middleware.py
+++ silvercare/middleware.py
@@ -1,16 +1,53 @@
 
-from channels.auth import AuthMiddlewareStack
+# from channels.auth import AuthMiddlewareStack
+# from channels.middleware import BaseMiddleware
+# from channels.db import database_sync_to_async
+# from django.contrib.auth.models import AnonymousUser
+# from django.contrib.sessions.models import Session
+# from django.contrib.auth import get_user_model
+
+# class SessionAuthMiddleware(BaseMiddleware):
+#     """
+#     Custom middleware that populates scope["user"] from Django session
+#     """
+#     async def __call__(self, scope, receive, send):
+#         # Get session key from cookies
+#         session_key = None
+#         headers = dict(scope.get('headers', []))
+#         if b'cookie' in headers:
+#             cookies = headers[b'cookie'].decode()
+#             for cookie in cookies.split('; '):
+#                 if cookie.startswith('sessionid='):
+#                     session_key = cookie.split('=')[1]
+#                     break
+
+#         if session_key:
+#             scope['user'] = await self.get_user_from_session(session_key)
+#         else:
+#             scope['user'] = AnonymousUser()
+
+#         return await super().__call__(scope, receive, send)
+
+#     @database_sync_to_async
+#     def get_user_from_session(self, session_key):
+#         try:
+#             session = Session.objects.get(session_key=session_key)
+#             user_id = session.get_decoded().get('_auth_user_id')
+#             User = get_user_model()
+#             return User.objects.get(id=user_id)
+#         except (Session.DoesNotExist, User.DoesNotExist):
+#             return AnonymousUser()
+
 from channels.middleware import BaseMiddleware
 from channels.db import database_sync_to_async
-from django.contrib.auth.models import AnonymousUser
-from django.contrib.sessions.models import Session
-from django.contrib.auth import get_user_model
+
 
 class SessionAuthMiddleware(BaseMiddleware):
     """
     Custom middleware that populates scope["user"] from Django session
     """
     async def __call__(self, scope, receive, send):
+        from django.contrib.auth.models import AnonymousUser
         # Get session key from cookies
         session_key = None
         headers = dict(scope.get('headers', []))
@@ -30,6 +67,9 @@ class SessionAuthMiddleware(BaseMiddleware):
 
     @database_sync_to_async
     def get_user_from_session(self, session_key):
+        from django.contrib.auth.models import AnonymousUser
+        from django.contrib.sessions.models import Session  # Delay import
+        from django.contrib.auth import get_user_model     # Delay import
         try:
             session = Session.objects.get(session_key=session_key)
             user_id = session.get_decoded().get('_auth_user_id')
